c69444acf5ac8c80aa3d976e631d5c8c59c6ddec
alexbkirk <alexbkirk@gmail.com>
2022-03-03 13:34:39 -0600

    added pwa

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton status="primary" *ngIf="!value">{{ value }}</button>
     <button nbButton hero status="success" *ngIf="value === 'X' ">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value === 'O' ">{{ value }}</button>
   `,
